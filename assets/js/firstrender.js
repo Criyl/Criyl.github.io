06f0551027ae7baad3cee3ba53f97597c7f969e6
ccarrollprojects <christopher.x.carroll@gmail.com>
2020-09-03 13:04:39 -0400

    add tracking plane

--- assets/js/firstrender.js
+++ assets/js/firstrender.js
@@ -1,106 +1,121 @@
-var distancefromsubject = 10;
-
-var scene = new THREE.Scene();
-
-var camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 100 );
-camera.position.z = distancefromsubject ;
-//camera.rotateOnAxis(new THREE.Vector3(0,1,0),1)
-
-var lightcolor = 0xFFFFFF
-
-var light = new THREE.PointLight(lightcolor,10,20,2);
-light.position.set(0,10,0);
-light.castShadow = true;
-
-scene.add(light);
-
-light.shadow.mapSize.width = 512;  // default
-light.shadow.mapSize.height = 512; // default
-light.shadow.camera.near = 0.5;       // default
-light.shadow.camera.far = 500      // default
-
-var renderer = new THREE.WebGLRenderer({ alpha: true ,antialias:true} );
-renderer.setClearAlpha(0)
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.shadowMap.enabled = true;
-
-var geometry = new THREE.BoxGeometry();
-
-var material = new THREE.MeshPhongMaterial( { color: 0xFF0000} );
-var cube1 = new THREE.Mesh( geometry, material );
-
-var transformation = new THREE.Matrix4();
-transformation.set(
-    1,0,0,2,
-    0,1,0,0,
-    0,0,1,0,
-    0,0,0,1
-)
-transformation.scale(new THREE.Vector3(1,2,1))
-cube1.applyMatrix4(transformation)
-cube1.castShadow = true;
-cube1.receiveShadow = true;
-scene.add( cube1 );
-
-material = new THREE.MeshPhongMaterial( { color: 0x0000FF} );
-var cube2 = new THREE.Mesh( geometry, material );
-cube2.receiveShadow=true;
-cube2.castShadow=true;
-scene.add( cube2);
-
-
-material = new THREE.MeshPhongMaterial( { color: 0x00FF00} );
-var cube3 = new THREE.Mesh( geometry, material );
-cube3.position.add(new THREE.Vector3(-2,0,0));
-cube3.castShadow = true;
-cube3.receiveShadow = true;
-scene.add( cube3);
-
-
-geometry = new THREE.PlaneGeometry(10,10)
-material = new THREE.MeshPhongMaterial( { color: 0xC6C6C6} );
-var plane = new THREE.Mesh( geometry, material );
-plane.rotateOnAxis(new THREE.Vector3(1,0,0),-Math.PI/2)
-plane.position.add(new THREE.Vector3(0,-4 ,0));
-plane.castShadow = true;
-plane.receiveShadow = true;
-scene.add( plane);
-
-
-
-var lighthelper = new THREE.PointLightHelper(light,0.5,lightcolor)
-scene.add(lighthelper)
 
 
 var timedelta = 0.1;
 var angle = 0;
+var count= 0;
+var direction = 1;
+var speed = 0.001;
+var lasttime = performance.now()
+function showFirst(id){
 
-function show(id){
+    var distancefromsubject = 1000;
 
-    var area = document.getElementById(id);
-    var width = area.scrollWidth;
-    area.appendChild( renderer.domElement );
+    var firstscene = new THREE.Scene();
 
-    renderer.setSize( width,width );
+    var firstcamera = new THREE.PerspectiveCamera( 75, 1, 0.1, 10000 );
+    firstcamera.position.z = distancefromsubject ;
+//firstcamera.rotateOnAxis(new THREE.Vector3(0,1,0),1)
+
+    var lightcolor = 0xFFFFFF
+
+    var light = new THREE.PointLight(lightcolor,10,2000,2);
+    light.position.set(0,1000,0);
+    light.castShadow = true;
+
+    firstscene.add(light);
+
+    light.shadow.mapSize.width = 512*100;  // default
+    light.shadow.mapSize.height = 512*100; // default
+    light.shadow.camera.near = 0.5;       // default
+    light.shadow.camera.far = 5000      // default
+
+    var firstrenderer = new THREE.WebGLRenderer({ alpha: true ,antialias:true} );
+    firstrenderer.setClearAlpha(0)
+    firstrenderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    firstrenderer.shadowMap.enabled = true;
+
+    var geometry = new THREE.BoxGeometry(100,100,100);
+
+    var material = new THREE.MeshPhongMaterial( { color: 0xFF0000} );
+    var cube1 = new THREE.Mesh( geometry, material );
+
+    var transformation = new THREE.Matrix4();
+    transformation.set(
+        1,0,0,200,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1
+    )
+    transformation.scale(new THREE.Vector3(1,2,1))
+    cube1.applyMatrix4(transformation)
+    cube1.castShadow = true;
+    cube1.receiveShadow = true;
+    firstscene.add( cube1 );
+
+    material = new THREE.MeshPhongMaterial( { color: 0x0000FF} );
+    var cube2 = new THREE.Mesh( geometry, material );
+    cube2.receiveShadow=true;
+    cube2.castShadow=true;
+    firstscene.add( cube2);
+
+
+    material = new THREE.MeshPhongMaterial( { color: 0x00FF00} );
+    var cube3 = new THREE.Mesh( geometry, material );
+    cube3.position.add(new THREE.Vector3(-200,0,0));
+    cube3.castShadow = true;
+    cube3.receiveShadow = true;
+    firstscene.add( cube3);
+
+
+    geometry = new THREE.PlaneGeometry(500,500)
+    material = new THREE.MeshPhongMaterial( { color: 0xC6C6C6} );
+    var firstplane = new THREE.Mesh( geometry, material );
+    firstplane.rotateOnAxis(new THREE.Vector3(1,0,0),-Math.PI/2)
+    firstplane.position.add(new THREE.Vector3(0,-400 ,0));
+    firstplane.castShadow = true;
+    firstplane.receiveShadow = true;
+    firstscene.add( firstplane);
+
+
+
+    var lighthelper = new THREE.PointLightHelper(light,0.5,lightcolor)
+    firstscene.add(lighthelper)
+
+
+    var firstarea = document.getElementById(id);
+    var width = firstarea.scrollWidth;
+    firstarea.appendChild( firstrenderer.domElement );
+
+    firstrenderer.setSize( width,width );
     var animate = function () {
         requestAnimationFrame( animate );
-        if(light.position.distanceTo(new THREE.Vector3(0,0,0)) > 11 )
-            timedelta *= -1;
-        light.position.add(new THREE.Vector3(1.5*timedelta,0,0))
+        timedelta = (performance.now() - lasttime)*speed;
+        lasttime = performance.now();
+
+
+        if(Math.abs(count) > 30 ) {
+			
+			count = direction*30;
+            direction *= -1;
+        }
+        count += direction*timedelta;
+
+        document.getElementById("debugfirst").innerText = "count: "+count+"\n timedelta: "+timedelta
         cube1.rotation.x += Math.abs(timedelta);
         cube3.rotation.y += Math.abs(timedelta);
-        distancefromsubject -= 3*timedelta
+        distancefromsubject -= 3*direction*timedelta
 
 
-        angle += timedelta;
-        camera.rotation.set(0,angle,0);
+        angle += direction*timedelta;
+        firstcamera.rotation.set(0,angle,0);
 
-        var x = distancefromsubject *Math.cos(angle)
-        var y = distancefromsubject *Math.sin(angle)
+        var x = distancefromsubject * Math.cos(angle)
+        var y = distancefromsubject * Math.sin(angle)
 
-        camera.position.set(y,0,x)
+        firstcamera.position.set(y,0,x)
 
-        renderer.render( scene, camera );
+        firstrenderer.render( firstscene, firstcamera );
+
     };
     animate();
 }
---
+++ assets/js/secondrender.js
@@ -0,0 +1,69 @@
+var distancetosubject=1000;
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 2000 );
+camera.position.z = distancetosubject;
+
+var renderer = new THREE.WebGLRenderer({ alpha: true ,antialias:true} );
+renderer.setClearAlpha(0)
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.enabled = true;
+
+//--lighting
+var lightcolor = 0xFFFFFF
+
+var light = new THREE.PointLight(lightcolor,10,2000,2);
+light.position.set(0,1000,distancetosubject);
+scene.add(light);
+
+light.shadow.mapSize.width = 512*100;  // default
+light.shadow.mapSize.height = 512*100; // default
+light.shadow.camera.near = 0.5;       // default
+light.shadow.camera.far = 5000      // default
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.enabled = true;
+
+var lighthelper = new THREE.PointLightHelper(light,200,lightcolor)
+scene.add(lighthelper)
+//--
+
+var geometry = new THREE.PlaneGeometry(500,500)
+var material = new THREE.MeshPhongMaterial( { color: 0xC6C6C6} );
+var plane = new THREE.Mesh( geometry, material );
+plane.castShadow = true;
+plane.receiveShadow = true;
+scene.add( plane);
+
+var timedelta = 0.1;
+
+window.onmousemove = getCursorXY;
+var dimensions = [window.innerWidth,window.innerHeight]
+var cursor = [-1,-1]
+function getCursorXY(event) {
+    cursor[0] = event.clientX;
+    cursor[1] = event.clientY;
+}
+
+function showSecond(id){
+
+    var area = document.getElementById(id);
+    var width = area.scrollWidth;
+    area.appendChild( renderer.domElement );
+    renderer.setSize( width,width );
+
+
+    var animate = function () {
+        requestAnimationFrame( animate );
+        areaRect = area.getBoundingClientRect();
+        middle = [areaRect.x+areaRect.width/2,areaRect.y+areaRect.height/2]
+        relativepos = [cursor[0]-middle[0],cursor[1]-middle[1]]
+        document.getElementById("debugsecond").innerText = "("+(relativepos[0])+","+(relativepos[1])+") "+"\n timedelta: "+timedelta
+        plane.setRotationFromAxisAngle( new THREE.Vector3(0,1,0), Math.atan((relativepos[0])/distancetosubject))
+        plane.rotateOnAxis( new THREE.Vector3(1,0,0), Math.atan((relativepos[1])/distancetosubject))
+
+        renderer.render( scene, camera );
+    };
+    animate();
+}
+
+
+
